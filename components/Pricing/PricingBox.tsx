82a9cd329d6c9c56d27e34f22311fbc748ae0e97
Ariel <128343170+tanggol69@users.noreply.github.com>
2023-10-30 16:58:52 +0800

    Update PricingBox.tsx

--- components/Pricing/PricingBox.tsx
+++ components/Pricing/PricingBox.tsx
@@ -24,9 +24,9 @@ const PricingBox = (props: {
         </div>
         <p className="mb-7 text-base text-body-color">{subtitle}</p>
         <div className="mb-8 border-b border-body-color border-opacity-10 pb-8 dark:border-white dark:border-opacity-10">
-          <button className="flex w-full items-center justify-center rounded-md bg-primary p-3 text-base font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp">
+          <a className="flex w-full items-center justify-center rounded-md bg-primary p-3 text-base font-semibold text-white transition duration-300 ease-in-out hover:bg-opacity-80 hover:shadow-signUp" href="/signin">
             Get Started
-          </button>
+          </a>
         </div>
         <div>{children}</div>
         <div className="absolute bottom-0 right-0 z-[-1]">
